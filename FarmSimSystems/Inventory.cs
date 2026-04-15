105a61c4aa07e9e6ff8cb7490bbc1ccf6cf7e52f
Pakaday <pakaday@gmail.com>
2026-04-14 21:33:57 -0400

    Created Inventory class using dictionary with methods for AddItem, RemoveItem, GetItem

---
+++ FarmSimSystems/Inventory.cs
@@ -0,0 +1,42 @@
+namespace FarmSimSystems;
+
+public class Inventory
+{
+    private Dictionary<int, Item> items = new Dictionary<int, Item>();
+
+    public void AddItem(Item item)
+    {
+        {
+            if (items.ContainsKey(item.Id))
+            {
+                items[item.Id].Quantity += item.Quantity;
+            }
+            else
+            {
+                items.Add(item.Id, item);
+            }
+        }
+    }
+
+    public void RemoveItem(Item item)
+    {
+        if (items.ContainsKey(item.Id))
+        {
+            items[item.Id].Quantity -= item.Quantity;
+
+            if (items[item.Id].Quantity <= 0)
+            {
+                items.Remove(item.Id);
+            }
+        }
+    }
+
+    public Item GetItem(int id)
+    {
+        if (items.ContainsKey(id))
+        {
+            return items[id];
+        }
+        return null;
+    }
+}
