b9a341797a5be9f72be35bfeaa1afa8de4fa364d
Pakaday <pakaday@gmail.com>
2026-04-17 19:02:29 -0400

    Added unit tests for inventory

---
+++ FarmSimSystems.Tests/InventoryTests.cs
@@ -0,0 +1,39 @@
+﻿using FarmSimSystems;
+
+namespace FarmSimSystems.Tests;
+
+public class InventoryTests
+{
+    [Fact]
+    public void AddItem_NewItem_AddsToInventory()
+    {
+        var inventory = new Inventory();
+        var wheat = new Item(1, "Wheat", 5, Rarity.Bronze);
+
+        inventory.AddItem(wheat);
+
+        Assert.Equal(5, inventory.GetItem(1).Quantity);
+    }
+
+    [Fact]
+    public void AddItem_ExistingItem_IncrementsQuantity()
+    {
+        var inventory = new Inventory();
+        var wheat = new Item(1, "Wheat", 1, Rarity.Bronze);
+
+        inventory.AddItem(wheat);
+        inventory.AddItem(wheat);
+
+        Assert.Equal(2, inventory.GetItem(1).Quantity);
+    }
+
+    [Fact]
+    public void RemoveItem_ReducesQuantity()
+    {
+        var inventory = new Inventory();
+        var wheat = new Item(1, "Wheat", 5, Rarity.Bronze);
+
+        inventory.AddItem(wheat);
+        inventory.RemoveItem(new Item(1, "Wheat", 2, Rarity.Bronze));
+    }
+}
