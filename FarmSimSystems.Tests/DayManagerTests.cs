48b8da5450582eb59383c4dfc058b7b0311cb83d
Frederick Neimeister <pakaday@me.com>
2026-04-20 14:23:41 -0400

    Added a DayManager and DayManagerTests class

---
+++ FarmSimSystems.Tests/DayManagerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmSimSystems.Tests
+{
+    public class DayManagerTests
+    {
+        [Fact]
+        public void NewDayManager_StartsOnDayOne()
+        {
+            // Arrange
+            Field field = new Field(2,2);
+            Inventory inventory = new Inventory();
+            DayManager dayManager = new DayManager(field, inventory);
+            // Act
+            int startingDay = dayManager.currentDay;
+            // Assert
+            Assert.Equal(1, startingDay);
+        }
+
+        [Fact]
+        public void AdvanceDay_IncrementsCurrentDay()
+        {
+            Field field = new Field(2, 2);
+            Inventory inventory = new Inventory();
+            DayManager dayManager = new DayManager(field, inventory);
+
+            int startingDay = dayManager.currentDay;
+            dayManager.AdvanceDay();
+
+            Assert.Equal(2, dayManager.currentDay);
+        }
+
+        [Fact]
+        public void AdvanceDay_AdvancesField()
+        {
+            Field field = new Field(2, 2);
+            Inventory inventory = new Inventory();
+            DayManager dayManager = new DayManager(field, inventory);
+
+            Plot plot = dayManager.field.GetPlot(0, 0);
+            plot.isWatered = true;
+            dayManager.AdvanceDay();
+
+            Assert.False(plot.isWatered);
+        }
+    }
+}
---
+++ FarmSimSystems/DayManager.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace FarmSimSystems
+{
+    public class DayManager
+    {
+        public Field field { get; }
+        public Inventory inventory { get; }
+        public int currentDay { get; set; }
+
+        public DayManager(Field field, Inventory inventory)
+        {
+            this.field = field;
+            this.inventory = inventory;
+            currentDay = 1;
+        }
+
+        public void AdvanceDay()
+        {
+            currentDay++;
+            field.AdvanceDay();
+        }
+    }
+}
