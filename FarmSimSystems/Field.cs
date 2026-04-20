d03bbdc67c9f3334a5c4c3239893879585191255
Pakaday <pakaday@gmail.com>
2026-04-19 21:11:52 -0400

    Added Field class to hold plots

---
+++ FarmSimSystems/Field.cs
@@ -0,0 +1,63 @@
+namespace FarmSimSystems;
+
+public class Field
+{
+    public int Rows { get; set; }
+    public int Cols { get; set; }
+    public Plot[,] plots;
+
+    public Field(int rows, int cols)
+    {
+        Rows = rows;
+        Cols = cols;
+        plots = new Plot[rows, cols];
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                plots[r, c] = new Plot();
+            }
+        }
+    }
+
+    public void GetPlot(int row, int col)
+    {
+        plots[row, col] = new Plot();
+    }
+
+    public void AdvanceDay(int rows, int cols)
+    {
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                plots[r, c].AdvanceDay();
+            }
+        }
+    }
+
+    public void ExpandField(int newRows, int newCols)
+    {
+        Plot[,] newPlots = new Plot[newRows, newCols];
+
+        for (int r = 0; r < newRows; r++)
+        {
+            for (int c = 0; c < newCols; c++)
+            {
+                if (r < Rows && c < Cols)
+                {
+                    newPlots[r, c] = plots[r, c];
+                }
+                else
+                {
+                    newPlots[r, c] = new Plot();
+                }
+            }
+        }
+
+        plots = newPlots;
+        Rows = newRows;
+        Cols = newCols;
+    }
+}
