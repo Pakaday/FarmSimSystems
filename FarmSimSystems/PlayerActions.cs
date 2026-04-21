527e371b49ad2b11aaa530a33ef06ca5ce19b2bd
Frederick Neimeister <pakaday@me.com>
2026-04-21 14:37:35 -0400

    Add PlayerActions methods

--- FarmSimSystems/PlayerActions.cs
+++ FarmSimSystems/PlayerActions.cs
@@ -8,6 +8,65 @@ namespace FarmSimSystems
 {
     public class PlayerActions
     {
+        public Field field { get; }
+        public Inventory inventory { get; }
 
+        public PlayerActions(Field field, Inventory inventory)
+        {
+            this.field = field;
+            this.inventory = inventory;
+        }
+
+        public void Till(int row, int col)
+        {
+            var plot = field.GetPlot(row, col);
+
+            if (plot.currentState == PlotState.Planted)
+            {
+                if (plot.currentCrop.currentStage != CropStage.Seed)
+                {
+                    return;
+                }
+            }
+
+            plot.Till(inventory);
+        }
+
+        public void Plant(int row, int col, SeedItem seed)
+        {
+            var plot = field.GetPlot(row, col);
+
+            if (plot.currentState != PlotState.Tilled)
+            {
+                return;
+            }
+
+            var crop = new Crop(seed.Name, seed.daysPerStage, new Item(seed.Id, seed.Name, 1, seed.Rarity));
+            plot.Plant(crop);
+            inventory.RemoveItem(seed);
+        }
+
+        public void Water(int row, int col)
+        {
+            var plot = field.GetPlot(row, col);
+
+            if (plot.isWatered)
+            {
+                return;
+            }
+
+            plot.Water();
+        }
+
+        public void Harvest(int row, int col)
+        {
+            var plot = field.GetPlot(row, col);
+
+            if (plot.currentState != PlotState.Planted || plot.currentCrop.currentStage != CropStage.Harvest)
+            {
+                return;
+            }
+            plot.Harvest(inventory);
+        }
     }
 }
