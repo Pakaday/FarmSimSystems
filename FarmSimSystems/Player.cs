3f2c2efef0302ef3cbd4719f8ee961c9a0b51007
Pakaday <pakaday@gmail.com>
2026-04-28 21:41:39 -0400

    Added constructor and methods for Player class

--- FarmSimSystems/Player.cs
+++ FarmSimSystems/Player.cs
@@ -13,4 +13,49 @@ public class Player
     public Tool currentTool { get; set; }
     public Position playerPosition { get; set; }
     public Direction facing { get; set; }
+
+    public Player(string name, int energy, int maxEnergy, IInventory inventory, int money, Tool currentTool,
+        Position playerPosition, Direction facing)
+    {
+        Name = name;
+        Energy = energy;
+        MaxEnergy = maxEnergy;
+        this.inventory = inventory;
+        Money = money;
+        this.currentTool = currentTool;
+        this.playerPosition = playerPosition;
+        this.facing = facing;
+    }
+
+    public void SetPosition(Position position)
+    {
+        playerPosition = position;
+    }
+
+    public void SetFacing(Direction direction)
+    {
+        facing = direction;
+    }
+
+    public void Sleep()
+    {
+        if (Energy < MaxEnergy)
+        {
+            Energy = MaxEnergy;
+        }
+    }
+
+    public void ConsumeEnergy(int amount)
+    {
+        Energy -= amount;
+        if (Energy < 0)
+        {
+            Energy = 0;
+        }
+    }
+
+    public bool CanAct()
+    {
+        return Energy > 0;
+    }
 }
