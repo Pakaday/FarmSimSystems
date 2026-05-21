5841a31f33e446ff3aab6bcb9011bc77308719ec
Fred Neimeister <Frederick.Neimeister@igs.com>
2026-05-21 13:45:25 -0400

    Add energy consumption tests for Till and Water

--- FarmSimSystems.Tests/PlayerActionsTests.cs
+++ FarmSimSystems.Tests/PlayerActionsTests.cs
@@ -19,9 +19,9 @@ namespace FarmSimSystems.Tests
             var playerActions = new PlayerActions(mockField.Object, CreateDefaultPlayer(mockInventory.Object));
             var plot = new Plot();
             mockField.Setup(f => f.GetPlot(0, 1)).Returns(plot);
-            
+
             playerActions.Till();
-            
+
             Assert.Equal(PlotState.Tilled, plot.currentState);
         }
 
@@ -35,9 +35,9 @@ namespace FarmSimSystems.Tests
             var plot = new Plot();
             mockField.Setup(f => f.GetPlot(0, 0)).Returns(plot);
             plot.Till(mockInventory.Object);
-            
+
             playerActions.Plant(0, 0, seedItem);
-            
+
             Assert.Equal(PlotState.Planted, plot.currentState);
             mockInventory.Verify(i => i.RemoveItem(seedItem), Times.Once);
         }
@@ -50,9 +50,9 @@ namespace FarmSimSystems.Tests
             var playerActions = new PlayerActions(mockField.Object, CreateDefaultPlayer(mockInventory.Object));
             var plot = new Plot();
             mockField.Setup(f => f.GetPlot(0, 1)).Returns(plot);
-            
+
             playerActions.Water();
-            
+
             Assert.True(plot.isWatered);
         }
 
@@ -69,12 +69,73 @@ namespace FarmSimSystems.Tests
             plot.Till(mockInventory.Object);
             plot.Plant(crop);
             crop.currentStage = CropStage.Harvest;
-            
+
             playerActions.Harvest(0, 0);
-            
+
             Assert.Equal(PlotState.Tilled, plot.currentState);
             mockInventory.Verify(i => i.AddItem(It.IsAny<Item>()), Times.Once);
+        }
 
+        [Fact]
+        public void Till_NotEnoughEnergy_DoesNotTill()
+        {
+            var mockField = new Mock<IField>();
+            var mockInventory = new Mock<IInventory>();
+            var player = CreateDefaultPlayer(mockInventory.Object);
+            player.Energy = 1;
+            var playerActions = new PlayerActions(mockField.Object, player);
+            var plot = new Plot();
+            mockField.Setup(f => f.GetPlot(0, 1)).Returns(plot);
+
+            playerActions.Till();
+
+            Assert.Equal(PlotState.Untilled, plot.currentState);
+        }
+
+        [Fact]
+        public void Water_NotEnoughEnergy_DoesNotWater()
+        {
+            var mockField = new Mock<IField>();
+            var mockInventory = new Mock<IInventory>();
+            var player = CreateDefaultPlayer(mockInventory.Object);
+            player.Energy = 1;
+            var playerActions = new PlayerActions(mockField.Object, player);
+            var plot = new Plot();
+            mockField.Setup(f => f.GetPlot(0, 1)).Returns(plot);
+
+            playerActions.Water();
+
+            Assert.False(plot.isWatered);
+        }
+
+        [Fact]
+        public void Till_ConsumesEnergy()
+        {
+            var mockField = new Mock<IField>();
+            var mockInventory = new Mock<IInventory>();
+            var player = CreateDefaultPlayer(mockInventory.Object);
+            var playerActions = new PlayerActions(mockField.Object, player);
+            var plot = new Plot();
+            mockField.Setup(f => f.GetPlot(0, 1)).Returns(plot);
+
+            playerActions.Till();
+
+            Assert.Equal(98, player.Energy);
+        }
+
+        [Fact]
+        public void Water_ConsumesEnergy()
+        {
+            var mockField = new Mock<IField>();
+            var mockInventory = new Mock<IInventory>();
+            var player = CreateDefaultPlayer(mockInventory.Object);
+            var playerActions = new PlayerActions(mockField.Object, player);
+            var plot = new Plot();
+            mockField.Setup(f => f.GetPlot(0, 1)).Returns(plot);
+
+            playerActions.Water();
+
+            Assert.Equal(98, player.Energy);
         }
     }
 }
